7ab30925827e0e5ee73b94a8cb172665113623b5
Nirklav <mrAnton911@gmail.com>
2021-05-08 20:55:02 +0300

    gpio 2 blink

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate sysfs_gpio;
+
+use sysfs_gpio::*;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+
+    let pin = Pin::new(2);
+    pin.with_exported(|| {
+        pin.set_direction(Direction::Out).unwrap();
+        loop {
+            pin.set_value(0).unwrap();
+            thread::sleep(Duration::from_millis(200));
+            pin.set_value(1).unwrap();
+            thread::sleep(Duration::from_millis(200));
+        }
+    }).unwrap();
 }
