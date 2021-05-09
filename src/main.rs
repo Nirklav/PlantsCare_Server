8feaee14d8f0e1f843e11848551338e1026b5cac
Nirklav <mrAnton911@gmail.com>
2021-05-09 12:52:22 +0300

    camera

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-extern crate sysfs_gpio;
-
 use sysfs_gpio::*;
-use std::thread;
+use rascam::*;
+
+use std::{thread, time};
 use std::time::Duration;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
+    let info = info().unwrap();
+    if info.cameras.len() < 1 {
+        println!("Found 0 cameras. Exiting");
+    }
+    else {
+        println!("{}", info);
+        simple_sync(&info.cameras[0]);
+    }
 
-    let pin = Pin::new(2);
+    /*let pin = Pin::new(2);
     pin.with_exported(|| {
         pin.set_direction(Direction::Out).unwrap();
         loop {
@@ -15,5 +25,18 @@ fn main() {
             pin.set_value(1).unwrap();
             thread::sleep(Duration::from_millis(200));
         }
-    }).unwrap();
+    }).unwrap();*/
+}
+
+fn simple_sync(info: &CameraInfo) {
+    let mut camera = SimpleCamera::new(info.clone()).unwrap();
+    camera.activate().unwrap();
+
+    let sleep_duration = time::Duration::from_millis(2000);
+    thread::sleep(sleep_duration);
+
+    let b = camera.take_one().unwrap();
+    File::create("image.jpg").unwrap().write_all(&b).unwrap();
+
+    println!("Saved image as image.jpg");
 }
